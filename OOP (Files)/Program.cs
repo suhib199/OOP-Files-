a1b600867bbf1b64c9eb901256e95f2a33c8d936
Suhib <alhanafisuhibe@gmail.com>
2023-12-10 15:32:44 +0300

    Add project files.

---
+++ OOP (Files)/Program.cs
@@ -0,0 +1,18 @@
+﻿string filePath = @"C:\Users\alhan\Desktop\FileTask.txt";
+
+FileStream readFrom = new FileStream(filePath, FileMode.Open);
+using StreamReader readNow = new StreamReader(readFrom);
+string pointer;
+int charactersNumber = 0;
+
+while ((pointer = readNow.ReadLine()) != null)
+{
+    var woredsInLine = pointer.Split("|");
+    foreach (var wored in woredsInLine)
+    {
+        charactersNumber += wored.Length;
+    }
+    Console.WriteLine(pointer);
+}
+Console.WriteLine($"The number of characters in file ={charactersNumber}");
+
